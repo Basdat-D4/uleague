4bf6b1f15cbc38f06024052a761880fbd0f9f789
arya.daffa1 <arya.daffa@gmail.com>
2023-05-07 18:37:49 +0700

    initial commit

--- rapat/urls.py
+++ rapat/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from rapat.views import show_rapat
 
 urlpatterns = [
-    path('', show_rapat, name='show_rapat'),
+    path('login/', show_rapat, name='show_rapat'),
 ]
