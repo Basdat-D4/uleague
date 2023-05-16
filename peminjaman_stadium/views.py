7f8632e0fb109e91cda4e530563c2f9be68816fd
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-05-16 21:41:55 +0700

    fetch list peminjaman stadium: success

--- peminjaman_stadium/views.py
+++ peminjaman_stadium/views.py
@@ -5,7 +5,35 @@ from django.db import connection
 
 # Create your views here.
 def show_list_pemesanan(request):
-    return render(request, "list_pemesanan.html")
+    cursor = connection.cursor()
+    cursor.execute(f'''
+        select s.nama, p.start_datetime, p.end_datetime
+        from stadium s, peminjaman p
+        where s.id_stadium = p.id_stadium;
+    ''')
+    pemesanan = cursor.fetchall()
+    # print(pemesanan)
+
+    # convert the data into a list of dictionaries
+    pemesanan_list = []
+    for stadium in pemesanan:
+
+        # change format of datetime
+        start_datetime = stadium[1]
+        end_datetime = stadium[2]
+        formatted_start_datetime = start_datetime.strftime('%d/%m/%Y')
+        formatted_end_datetime = end_datetime.strftime('%d/%m/%Y')
+
+        pemesanan_list.append({
+            'nama': stadium[0],
+            'start_datetime': formatted_start_datetime,
+            'end_datetime': formatted_end_datetime,
+        })
+
+    connection.close()
+
+    context = {'pemesanan_list': pemesanan_list}
+    return render(request, "list_pemesanan.html", context)
 
 def show_pilih_stadium(request):
     # response = HttpResponse()
