a02d25f77ec5d880cb6c1fa904c391d88b0b6576
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-05-16 18:54:39 +0700

    fetch stadium name: success

--- peminjaman_stadium/views.py
+++ peminjaman_stadium/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.db import connection
 
 
 # Create your views here.
@@ -6,7 +8,24 @@ def show_list_pemesanan(request):
     return render(request, "list_pemesanan.html")
 
 def show_pilih_stadium(request):
-    return render(request, "pilih_stadium.html")
+    # response = HttpResponse()
+
+    cursor = connection.cursor()
+    cursor.execute(f'''
+        SELECT NAMA
+        FROM STADIUM
+    ''')
+    stadium_list = cursor.fetchall()
+    # response.write(stadium_list)
+    connection.close()
+
+    names_list = []
+    for stadium in stadium_list:
+        names_list.append(stadium[0])
+
+    context = {'stadium_names': names_list}
+    print(context)
+    return render(request, "pilih_stadium.html", context)
 
 def show_list_waktu(request):
     return render(request, "list_waktu.html")
