006c17086ee3c85624e9880aa0f3620b38921cc7
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-05-23 16:54:43 +0700

    Revert "Syifa"

--- history_rapat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HistoryRapatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'history_rapat'
--- history_rapat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from history_rapat.views import show_history_rapat
-
-
-urlpatterns = [
-    path('', show_history_rapat, name='show_history_rapat'),
-    
-]
