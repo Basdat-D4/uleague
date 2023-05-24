fc72d9ca7512847d557578a5b5534601fd414988
uripsubagyo <uripsubagyo@Is-MacBook-Pro-2.local>
2023-05-25 00:10:36 +0700

    functional tambah pertandingan

--- pembuatan_pertandingan/views.py
+++ pembuatan_pertandingan/views.py
@@ -3,20 +3,72 @@ from django.shortcuts import redirect
 from supabase import *
 import requests
 import uuid
-
 # Create your views here.
 def show_home_pembuatan_pertandingan(request):
+    # data = execute_query(f'''
+    #     SELECT TIM1.id_pertandingan, CONCAT(TIM1.nama_tim, ' VS ', TIM2.nama_tim)
+    #     FROM TIM_PERTANDINGAN TIM1
+    #     JOIN PERTANDINGAN P ON TIM1.id_pertandingan = P.id_pertandingan
+    #     JOIN TIM_PERTANDINGAN TIM2 ON TIM2.id_pertandingan = TIM1.id_pertandingan
+    #     WHERE TIM2.nama_tim != TIM1.nama_tim
+    #     GROUP BY TIM1.id_pertandingan;
+    # ''')
 
-    return render(request, "home_pembuatan_pertandingan.html")
+    data = execute_query(f'''
+            SELECT DISTINCT TIM3.id_pertandingan, CONCAT(temp2.tim_1, ' VS ', TIM3.nama_tim)
+            FROM tim_pertandingan TIM3 
+            natural join (
+                SELECT TIM1.id_pertandingan, TIM1.nama_tim as tim_1
+                FROM PERTANDINGAN P
+                NATURAL JOIN tim_pertandingan TIM1
+            ) as TEMP2
+            where TIM3.nama_tim != tim_1
+    ''')
 
-def select_stadium(request):
-    # #TODO:
-    myuuid = uuid.uuid4()
-    myuuid = str(myuuid)
-    newuuid = "0A" + myuuid[2:len(myuuid)]
-    print('Your UUID is: ' + str(myuuid))
-    print('Your UUID NEW: ' + newuuid)
+    ST = ' '
+    ST = ST.join(map(str, data))
+    result = ST.split(") (")
+    DATA =[]
+    print(ST)
+    for item in result:
+        items = item.split("', '")
+        print("result" + str(items))
+        byte = [
+            items[0].replace("'", "").replace("(", "").replace(")",""), 
+            items[1].replace("'", "").replace("(","").replace(")", "")
+        ]
+        print(byte)
+        DATA.append(byte)
+    #Struktur :
+    # [
+    #   [[GRUP A], [GRUP B]]
+    # ]
+    REMOVE_DATA = []
+    DATA_A = []
+    DATA_B = [] 
+    for i in DATA:
+        # print(i[1])
+        if i[0][0:2] == "0a":
+            if i[0] in REMOVE_DATA:
+                continue
+            else:
+                REMOVE_DATA.append(i[0])
+                DATA_A.append(i)
+        else:
+            if i[0] in REMOVE_DATA:
+                continue
+            else:
+                REMOVE_DATA.append(i[0])
+                DATA_B.append(i)
 
+    return render(request, "home_pembuatan_pertandingan.html", {
+        'first_data': DATA_A,
+        'second_data': DATA_B,
+        'first_data': DATA_A,
+        'second_data': DATA_B,
+    })
+
+def select_stadium(request, group):
 
     fetch_data = execute_query("SELECT universitas FROM TIM;")
     fetch_data = list(fetch_data)
@@ -27,34 +79,57 @@ def select_stadium(request):
     for item in result:
         newItem = item.replace("',", "").replace("'", "").replace("(", "").replace(")", "")
         DATA.append(newItem)
-    print(DATA)
 
-    if 'stadium' in request.session:
-            print("remove")
-            request.session.clear()
-    
+
     if request.method == "POST":
-        title_task = request.POST.get("stadium")
-        description_task = request.POST.get('tanggal')
+        stadium = request.POST.get("stadium")
+        tanggal = request.POST.get('tanggal')
 
-        request.session['stadium'] =  str(title_task)
-        print(str(request.session.get('stadium')))
-  
-        requests.session.modified = True
-        return redirect(
-            'pembuatan_pertandingan:select_time_stadium')
+        # request.session['stadium'] =  str(title_task)
+        # print(str(request.session.get('stadium')))
+        # requests.session.modified = True
+
+        context = {
+            'group' : group,
+            'stadium' : stadium,
+            'tanggal' : tanggal,
+        }
+        return redirect('pembuatan_pertandingan:select_time_stadium', str(context))
     
-    return render(request, "stadium_and_date.html", {"daftar_stadium": DATA})
+    return render(request, "stadium_and_date.html", {
+        "daftar_stadium": DATA,
+        "context": {
+            'group' : group
+        }
+        })
 
-def select_time_stadium(request):
+def select_time_stadium(request, data):
+    data = eval(data)
+    print("INI DATA DARI SELECT TIMER STADIUM")
+    print(str(data))
 
-    return render(request, "select_time.html", 
-                  context={
-                       'stadium': str(request.session.get('stadium')),
-                       })
+    
+    # GET DATA STADIUM YANG DAPAT DIGUNAKAN
+    # DATA_FETCH = execute_query(f'''
+        
+    # ''')
+    return render(request, "select_time.html", {
+        'result' : [
+            {'stadium' : data['stadium'],
+            'waktu' : '07.00 - 09.00',
+            'status': False
+             },
+            {'stadium' : data['stadium'],
+            'waktu' : '11.00 - 13.00',
+            'status': True
+             },
+        ]
+        
+    })
 
 
-def tambah_pertandingan(request):
+def tambah_pertandingan(request, data):
+    print(eval(data))
     #GET DATA WASIT
     fetch_data_wasit = execute_query("""
         SELECT N.ID, N.Nama_depan, N.nama_belakang 
@@ -78,15 +153,52 @@ def tambah_pertandingan(request):
 
     #GET DATA DARI TIM YANG SUDAH MENDAFTAR LAPANGAN
     fetch_data_tim = execute_query("""
-        SELECT nama_tim 
-        FROM TIM;
+        SELECT DISTINCT tim.nama_tim
+        FROM TIM
+        JOIN tim_manajer MAN on MAN.nama_tim = TIM.nama_tim
+        JOIN peminjaman PEM ON PEM.id_manajer = MAN.id_manajer
+        WHERE '2023-05-06' BETWEEN PEM.start_datetime and PEM.end_datetime
         """)
+
     fetch_data_tim = list(fetch_data_tim)
+    print(fetch_data_tim)
     result_data_tim =[]
     for items in fetch_data_tim:
         temp = str(items).replace(",", "").replace("'","").replace("(", "").replace(")","")
         result_data_tim.append(temp)
-    #TODO
+    #TODO: LOGIC INSERT DATA
+    uudi_pertandingan = uuid.uuid4()
+    uudi_pertandingan = str(uudi_pertandingan)
+    print(uudi_pertandingan)
+    print("0a"+uudi_pertandingan[2:])
+
+
+    if request.method == "POST":
+        WASIT_UTAMA = request.POST.get("wasit-utama")
+        WASIT_PEMBANTU_1 = request.POST.get("wasit-pembantu-1")
+        WASIT_PEMBANTU_2 = request.POST.get("wasit-pembantu-2")
+        WASIT_CADANGAN = request.POST.get("wasit-cadangan")
+
+        TIM_1 = request.POST.get("tim-1")
+        TIM_2 = request.POST.get("tim-2")
+        
+        # uudi_pertandingan = uuid.uuid4()
+        uudi_pertandingan = uuid.uuid4()
+        uudi_pertandingan = str(uudi_pertandingan)
+        uudi_pertandingan = '0b' + uudi_pertandingan[2:]
+
+        # execute_query('''
+        #     INSERT INTO PERTANDINGAN (ID_PERTANDINGAN, Start_DateTime, End_Datetime, Stadium) VALUES ('{uudi_pertandingan}', '2023-05-01 8:00:00', '2023-05-01 10:00:00', 'e54965f3-f636-472a-a089-74ff111f8c1b');
+        # ''')
+
+        # INSERT TIM PERTANDINGAN
+        # execute_query('''
+        # ''')
+
+        # INSERT WASIT BERTUGAS PERTANDINGAN
+        # execute_query('''
+        # ''')
+        
     return render(request, "fix_match.html", 
                   {'daftar_wasit': result_data_wasit,
                    'daftar_tim': result_data_tim
