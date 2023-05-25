2c877cf3fe4541c2314bf114f6c106f276e8636d
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-05-25 15:38:56 +0700

    CR Peminjaman Stadium: Complete

--- peminjaman_stadium/views.py
+++ peminjaman_stadium/views.py
@@ -54,6 +54,8 @@ def show_pilih_stadium(request):
 def show_list_waktu(request):
     date = request.POST.get('date')
     id_stadium = request.POST.get('id_stadium')
+    username = request.COOKIES['username']
+    print(username)
     print(date)
     print(id_stadium)
 
@@ -64,9 +66,77 @@ def show_list_waktu(request):
         WHERE id_stadium = '{id_stadium}'
     ''')
     nama_stadium = cursor.fetchmany(1)[0][0]
+    cursor.close()
+
+    cursor = connection.cursor()
+
+    # cek jadwal peminjaman
+    cursor.execute(f'''
+        SELECT ID_Stadium
+        FROM Stadium
+        WHERE ID_Stadium = '{id_stadium}'
+        AND ID_Stadium NOT IN (
+            SELECT ID_Stadium
+            FROM PEMINJAMAN
+            WHERE '{date}' BETWEEN Start_DateTime AND End_DateTime
+        );
+    ''')
+
+    idStadiumFreePeminjaman = cursor.fetchmany(1)
+    if idStadiumFreePeminjaman != []:
+        idStadiumFreePeminjaman = idStadiumFreePeminjaman[0][0]
+    else:
+        return render(request, "not_available.html", {'nama_stadium': nama_stadium})
+    cursor.close()
+
+    # cek jadwal pertandingan
+    cursor = connection.cursor()
+    cursor.execute(f'''
+        SELECT ID_Stadium
+        FROM Stadium
+        WHERE ID_Stadium = '{id_stadium}'
+        AND ID_Stadium NOT IN (
+            SELECT ID_Stadium
+            FROM PERTANDINGAN
+            WHERE '{date}' BETWEEN Start_DateTime AND End_DateTime
+        );
+    ''')
+    idStadiumFreePertandingan = cursor.fetchmany(1)
+    if idStadiumFreePertandingan != []:
+        idStadiumFreePertandingan= idStadiumFreePertandingan[0][0]
+    else:
+        return render(request, "not_available.html", {'nama_stadium': nama_stadium})
+    cursor.close()
+
+    print(idStadiumFreePertandingan)
+    print(idStadiumFreePeminjaman)
+    
+    # convert uuid to string
+    idStadiumFreePertandingan = str(idStadiumFreePertandingan)
+    idStadiumFreePeminjaman = str(idStadiumFreePeminjaman)
+
+    # isAvailable = 'Not Available'
+    # if id_stadium == idStadiumFreePertandingan and id_stadium == idStadiumFreePeminjaman:
+    #     isAvailable = 'Available'
+
+    # fetch id_manajer
+    cursor = connection.cursor()
+    cursor.execute(f'''
+        SELECT id_manajer
+        FROM manajer
+        WHERE username = '{username}'
+    ''')
+    id_manajer = cursor.fetchmany(1)[0][0]
+
+    cursor = connection.cursor()
+    cursor.execute(f'''
+        INSERT INTO PEMINJAMAN (id_manajer, start_datetime, end_datetime, id_stadium)
+        VALUES ('{id_manajer}', '{date} 05:00:00', '{date} 11:00:00', '{id_stadium}')
+    ''')
 
     context = {
-        'nama_stadium': nama_stadium
+        'nama_stadium': nama_stadium,
+        'date': date
     }
 
     return render(request, "list_waktu.html", context)
