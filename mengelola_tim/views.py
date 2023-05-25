f92721ce2c127ff37315052d6c2264c2e42028a9
Ayu Putri Dewi Fitriyani <ayu.putri13>
2023-05-26 00:37:03 +0700

    bismillah

--- mengelola_tim/views.py
+++ mengelola_tim/views.py
@@ -6,61 +6,6 @@ import uuid
 from django.http import HttpResponseForbidden, HttpResponseRedirect
 
 
-
-def regis_tim(request):
-    # if request.session['role'] == "Manajer":
-    #     with connection.cursor() as cursor:
-    #         username_Manajer = request.COOKIES['username']
-    #             #TAMPILIN PEMAIN
-    #         cursor.execute ( f'''
-    #             SELECT t.nama_tim, m.id_manajer
-    #             FROM MANAJER m ,TIM_MANAJER t
-    #             WHERE m.username = '{username_Manajer}' AND m.id_manajer = t.id_manajer;
-    #         ''')
-            
-    #         namaTimManajer = fetch(cursor)
-    #         # print (namaTimManajer)
-    #         try:
-    #             id_manajer = namaTimManajer[0]['id_manajer']
-    #             namaTimManajer = namaTimManajer[0]["nama_tim"]
-    #             if namaTimManajer:
-    #                 return redirect("/mengelola_tim/tim")
-    #         except Exception as e:
-    #             if request.method == 'POST':
-    #                 namaTimManajer = request.POST.get('team_name')
-    #                 uni = request.POST.get('university_name')
-                
-    #                 cursor.execute(
-    #                     f"""
-    #                     SELECT id_manajer
-    #                     FROM MANAJER
-    #                     WHERE username = '{username_Manajer}'
-    #                     ;
-    #                     """
-    #                 )
-    #                 id_manajer = fetch(cursor)[0]['id_manajer']
-
-    #                 cursor.execute(
-    #                     f"""
-    #                     INSERT INTO TIM (nama_tim, universitas)
-    #                     VALUES ('{namaTimManajer}', '{uni}')
-    #                     ;
-    #                     """
-    #                 )
-    #                 connection.commit()
-    #                 cursor.execute(
-    #                     f"""
-    #                     INSERT INTO TIM_MANAJER (id_manajer, nama_tim)
-    #                     VALUES ('{id_manajer}', '{namaTimManajer}')
-    #                     ;
-    #                     """
-    #                 )
-    #                 connection.commit()
-    #                 return redirect("/mengelola_tim/tim")
-    # else:
-    #     return HttpResponseForbidden("Forbidden")             
-    return render(request, "regis_tim.html")
-
 # Create your views here.
 def show_tim(request):
     # dict_response = {'data_pemain':[]}
@@ -231,97 +176,63 @@ def daftar_pemain(request):
 def daftar_pelatih(request):
     return render(request, "daftar_pelatih.html")
 
+
+
+def regis_tim(request):
+    if request.session['role'] == "Manajer":
+        with connection.cursor() as cursor:
+            username_Manajer = request.COOKIES['username']
+                #TAMPILIN PEMAIN
+            cursor.execute ( f'''
+                SELECT t.nama_tim, m.id_manajer
+                FROM MANAJER m ,TIM_MANAJER t
+                WHERE m.username = '{username_Manajer}' AND m.id_manajer = t.id_manajer;
+            ''')
+            
+            namaTimManajer = fetch(cursor)
+            # print (namaTimManajer)
+            try:
+                id_manajer = namaTimManajer[0]['id_manajer']
+                namaTimManajer = namaTimManajer[0]["nama_tim"]
+                if namaTimManajer:
+                    return redirect("/mengelola_tim/tim")
+            except Exception as e:
+                if request.method == 'POST':
+                    namaTimManajer = request.POST.get('team_name')
+                    uni = request.POST.get('university_name')
+                
+                    cursor.execute(
+                        f"""
+                        SELECT id_manajer
+                        FROM MANAJER
+                        WHERE username = '{username_Manajer}'
+                        ;
+                        """
+                    )
+                    id_manajer = fetch(cursor)[0]['id_manajer']
+
+                    cursor.execute(
+                        f"""
+                        INSERT INTO TIM (nama_tim, universitas)
+                        VALUES ('{namaTimManajer}', '{uni}')
+                        ;
+                        """
+                    )
+                    connection.commit()
+                    cursor.execute(
+                        f"""
+                        INSERT INTO TIM_MANAJER (id_manajer, nama_tim)
+                        VALUES ('{id_manajer}', '{namaTimManajer}')
+                        ;
+                        """
+                    )
+                    connection.commit()
+                    return redirect("/mengelola_tim/tim")
+    else:
+        return HttpResponseForbidden("Forbidden")             
+    return render(request, "regis_tim.html")
+
+
 def fetch(cursor):
     columns = [col[0] for col in cursor.description]
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
-# ==========================================================================================
-
-# def create_regis_tim(request):
-#     if (request.session.get('username') == None):
-#         return redirect('/login/')
-#     if (request.session.get('nama_tim') != None):
-#         return redirect('/manajer/tim/')
-#     if (request.method == 'POST'):
-#         nama_tim = request.POST.get('nama_tim')
-#         universitas = request.POST.get('universitas')
-
-#         response = query(f''' INSERT INTO TIM (nama_tim, universitas) VALUES ('{nama_tim}', '{universitas}') ''')
-#         if (isinstance(response, Exception)):
-#             context = {'message': "Nama tim sudah terdaftar"}
-#             return render(request, 'registerTim.html', context)
-        
-#         id = query(f'''
-#             SELECT id_manajer FROM MANAJER WHERE username = '{request.session.get('username')}'
-#             ''')[0]['id_manajer'] 
-#         response = query(f'''
-#             INSERT INTO TIM_MANAJER (id_manajer, nama_tim)
-#             VALUES ('{id}', '{nama_tim}')
-#             ''' )
-#         print(response)
-#         request.session['nama_tim'] = nama_tim
-#         return redirect('/manajer/kelolatim/')
-#     return render(request, "regis_tim.html")
-
-
-# def update_daftar_pemain(request):
-#     if (request.session.get('username') == None):
-#         return redirect('/authentication/login/')
-#     if(request.session.get('user_role') != 'manajer'):
-#         if (request.session.get('user_role') == None):
-#             return redirect('/authentication/login/')
-#         return redirect(f'/{request.session.get("user_role")}/')
-#     list_pemain = query(f''' SELECT * FROM PEMAIN WHERE nama_tim IS NULL ''')
-#     context = {'list_pemain': list_pemain}
-#     if (request.method == 'POST'):
-#         query(f''' UPDATE PEMAIN SET nama_tim = '{request.session.get('nama_tim')}' WHERE id_pemain = '{request.POST.get('pemain')}' ''')
-#         return redirect('/manajer/kelolatim/')
-#     return render(request, "daftar_pemain.html")
-
-
-# def update_daftar_pelatih(request):
-#     if (request.session.get('username') == None):
-#         return redirect('/authentication/login/')
-#     if(request.session.get('user_role') != 'manajer'):
-#         if (request.session.get('user_role') == None):
-#             return redirect('/authentication/login/')
-#         return redirect(f'/{request.session.get("user_role")}/')
-#     list_pelatih = query(f''' SELECT p.id_pelatih, nama_depan, nama_belakang, string_agg(spesialisasi, ', ') as sp
-#     FROM non_pemain np
-#     JOIN pelatih p ON np.id = p.id_pelatih
-#     JOIN spesialisasi_pelatih sp ON p.id_pelatih = sp.id_pelatih
-#     WHERE p.nama_tim IS NULL
-#     GROUP BY p.id_pelatih, nama_depan, nama_belakang ''')
-
-#     context = {'list_pelatih': list_pelatih}
-#     if (request.method == 'POST'):
-#         print(request.POST.get('id'))
-#         response = query(f''' UPDATE PELATIH SET nama_tim = '{request.session.get('nama_tim')}' WHERE id_pelatih = '{request.POST.get('id')}' ''')
-#         if (isinstance(response, Exception)):
-#             context['message'] = response.args[0].split("\n")[0]
-#             return render(request, 'registerPelatih.html', context)
-#         else: 
-#             return redirect('/manajer/kelolatim/')
-#     return render(request, "daftar_pelatih.html")
-    
-
-
-            
-
-    # queryManajer = f'''
-    #     SELECT id_manajer
-    #     FROM Manajer
-    #     WHERE username = '{username_Manajer}'
-    #     '''
-    # cursor = connection.cursor()
-    # cursor.execute(queryManajer)
-    # id_manajer = cursor.fetchone()[0]
-    
-    
-    # queryNamaTim = f'''
-    #     SELECT nama_tim 
-    #     FROM TIM_MANAJER 
-    #     WHERE id_manajer = '{id_manajer}'
-    #     '''
-    # cursor = connection.cursor()
-    # cursor.execute(queryNamaTim)
-    # namaTim = cursor.fetchone()[0]
